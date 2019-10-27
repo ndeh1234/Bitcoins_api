8fc926dfbd79e26c48caaa4ed8f5861ca3287497
ndeh1234 <30512965+ndeh1234@users.noreply.github.com>
2019-10-27 01:37:56 -0500

    Update test_Bitcoins.py
    Added a patch() decorator

--- test_Bitcoins.py
+++ test_Bitcoins.py
@@ -1,22 +1,15 @@
-
-import unittest
-from unittest import TestCase
 from unittest.mock import patch
+import bitcoinAPI
 
+class TestBitCoins(TestCase):
 
-
-import Bitcoins
-
-
-class TestPriceOfBitcoins(TestCase):
-
-
- def test_get_price_of_bitcoins(self,mock_rates):
-     mock_rates = 8,692.8000
-     example_api_response = {'base': 'USD', 'date': '2019-02-04', 'rates': {'USD':mock_rates}}
-     mock_rates.side_effect = [example_api_response]
-     converted = Bitcoins.mock_rates(8692.8, 'USD')
-     self.assertEqual(8692.8, converted)
+    @patch('bitcoinAPI.api')
+    def test_convert(self, mock_rates):
+        mock_rate = 15.34
+        example_api_response = {"description":"United States Dollar","rate_float":mock_rate}
+        mock_rates.side_effect = [example_api_response]
+        converted = bitcoinAPI.convert(100, mock_rate)
+        self.assertEqual(1534, converted)
 
 
 if __name__ == '__main__':
